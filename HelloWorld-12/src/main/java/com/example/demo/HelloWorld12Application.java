cdb88e9bffe8aa034876f7456b4710e1e9daf387
AE205_PC2 <AE205_PC2@Infva06863>
2019-08-20 18:08:00 -0700

    test

---
+++ HelloWorld-12/src/main/java/com/example/demo/HelloWorld12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorld12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorld12Application.class, args);
+	}
+
+}
